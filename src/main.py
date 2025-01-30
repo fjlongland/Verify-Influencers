06d912b110ee4a8b5f9480eef827716aabd9c6fb
fjlongland <fjlongland@gmail.com>
2025-01-30 17:00:19 +0200

    stillll having issues with railway

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import os
 import sys
 from fastapi import FastAPI
 #from PyQt5.QtWidgets import QApplication
-from .frontend import MainWindow
+#from .frontend import MainWindow
 from .calls import basic_call
 from .routers import router
 from fastapi.middleware.cors import CORSMiddleware
@@ -27,11 +27,11 @@ def start_api():
     port = int(os.getenv("PORT", 8000))
     uvicorn.run("src.main:api", host="0.0.0.0", port=port, reload=True)
 
-def start_app():
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-    sys.exit(app.exec_())
+#def start_app():
+    #app = QApplication(sys.argv)
+    #window = MainWindow()
+    #window.show()
+    #sys.exit(app.exec_())
 
 def main():
 
